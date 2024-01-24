638597909d567c4e47070d014b1fe83b61f2d2f0
Nekroid <philipleinung@aol.de>
2024-01-24 10:57:53 +0100

    cat / cat2

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,9 +2,18 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        // Objekt ensteht...
+        Cat cat = new Cat(); // Instanzierung
+        System.out.println(cat);
+        cat.tellYourAddress();
 
+        System.out.println("------------");
 
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
     }
 
    
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Cat {
+
+
+    public void sayHello()
+    {
+        System.out.println("hello");   
+    }
+
+    void tellYourAddress()
+    {
+        System.out.println(this);
+    }
+}
